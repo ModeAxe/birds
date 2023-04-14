d2ee0563ceefa0c5c5a6b5f15157aed9348e451c
Elijah Temwani Zulu <modeaxe@Gmail.com>
2023-04-13 20:03:16 -0400

    wireframe

--- Assets/GenerativeSpawn.cs
+++ Assets/GenerativeSpawn.cs
@@ -7,12 +7,15 @@ public class GenerativeSpawn : MonoBehaviour
     public GameObject bird;
     public GameObject dome;
     private List<Vector3> birds_pos = new List<Vector3>();
+    private List<GameObject> birds = new List<GameObject>();
     void Start()
     {
         //get mesh positions
         Mesh domeMesh = dome.GetComponent<MeshFilter>().mesh;
 
-        for (int i = 0; i < domeMesh.vertices.Length; i++)
+        Debug.Log(domeMesh.vertices.Length);
+
+        for (int i = 0; i < 341; i++)
         {
             
             birds_pos.Add(dome.transform.TransformPoint(domeMesh.vertices[i]));
@@ -20,12 +23,21 @@ public class GenerativeSpawn : MonoBehaviour
         //instatiate birds
         foreach (Vector3 position in birds_pos)
         {
-            GameObject.Instantiate(bird, position, Quaternion.identity);
+            birds.Add(GameObject.Instantiate(bird, position, Quaternion.identity));
         }
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(birds.Count);
+            for (int i = 0; i < 50; i++)
+            {
+                Destroy(birds[i]);
+                Debug.Log("Gone");
+            }
+
+        }  
     }
 }
